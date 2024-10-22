71cf8d69f4470fd92ba14bfbb3ac48a7fc470c86
xaqan05 <xaqan2005@gmail.com>
2024-10-22 18:15:14 +0400

    version1

---
+++ Task1/Task1/Helpers/ExtentionMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Helpers
+{
+    public static class ExtentionMethods
+    {
+        public static string toCapitalize(this string str)
+        {
+            return char.ToUpper(str[0]) + str.Substring(1);   
+        }
+
+        public static int[] GetValueIndexes(this string str, char c)
+        {
+            int[] arr = new int[0];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c)
+                {
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length - 1] = i;
+                }
+            }
+            return arr;
+        }
+
+     
+    }
+}
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Task1.Helpers;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "zxa qan";
+
+           int[] arr =  str.GetValueIndexes('a');
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
